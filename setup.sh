836fe451bb05c80022d1a132205326c9b022f48c
Daniel Rojas <l20j45_@gmail.com>
2021-11-19 12:43:17 -0600

    modifique un error

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+sudo apt install pv
+sudo apt install p7zip-full
+sudo apt install p7zip-rar
+sudo apt install speedtest-cli
+sudo apt install youtube-dl
+sudo apt install ffmpeg
+sudo apt install net-tools
+sudo apt install unzip
+sudo apt install python3-pip
+sudo apt install xlsx2csv
+sudo apt install ghostscript
+sudo pip install mysql-connector-python
+sudo pip install pyperclip
+sudo pip install termcolor
